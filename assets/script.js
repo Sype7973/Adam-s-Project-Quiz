20d5c2dee30fcb3e900f3a736cf187e979192663
unknown <adam.day7973@gmail.com>
2023-03-05 12:27:25 +1100

    built html page; added css elements for body,main, added highscores html page for local storage render

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// declare global variables needed for entire game and questions array
+var currentQuestionNumber = 0;
+var time = questions.length * 25;
+var timerId;
+
+// declare element ids that will need to be selected throughout functions to create content
+var quizTimerElement = document.getElementById("time");
+var quizScoresElement = document.getElementById("quiz-scores");
+var questioChoicesElement = document.getElementById("question-choices");
+var quizSubmitButton = document.getElementById("submit");
+var playerInitialsElement = document.getElementById("initials");
+var startGameElement = document.getElementById("gamestart");
+var
+
+
+// start quiz function
+
+
+
+
+// timer function
+
+
+
+
+// Questions function and go to next question
+
+
+
+
+// Incorrect and correct functions - move to next question; need timer reset function
